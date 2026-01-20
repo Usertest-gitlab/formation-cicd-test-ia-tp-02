227e663f12ffcc1d4fc05a015788ae9d5ef02327
Usertest-gitlab <arthur.migne444@gmail.com>
2026-01-20 16:51:11 +0100

    Update UserServiceTest.java

--- src/test/java/cicd/user/UserServiceTest.java
+++ src/test/java/cicd/user/UserServiceTest.java
@@ -1,6 +1,8 @@
 package cicd.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -13,12 +15,24 @@ import com.devops.cicd.user.UserService;
 class UserServiceTest {
 
     @Test
-    void registerTest() {
+    void registerWorkTest() {
         UserService service = new UserService();
 
         User user = service.register("john@doe.com", "Abcdef1!", Role.USER);
 
         assertNotNull(user);
         assertTrue(user instanceof User);
+    }
+
+    @Test
+    void registerReturnEmailExceptionTest() {
+        UserService service = new UserService();
+        
+        IllegalArgumentException ex = assertThrows(
+            IllegalArgumentException.class,
+            () -> service.register("@doe.com", "Abcdef1!", Role.USER)
+        );
+
+        assertEquals("email must be valid", ex.getMessage());
     }
 }
